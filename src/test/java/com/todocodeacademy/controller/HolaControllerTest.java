0f3f0f37382e402167a231dce2b74c526ecccc26
Junior <andresjunior240498hehe@gmail.com>
2025-09-26 18:02:56 -0400

    github actions project

---
+++ src/test/java/com/todocodeacademy/controller/HolaControllerTest.java
@@ -0,0 +1,24 @@
+package com.todocodeacademy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HolaController.class)
+class HolaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void deberiaResponderHolaMundo() throws Exception {
+        mvc.perform(get("/hola"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hola Mundo"));
+    }
+
+}
